015d3a87d1488cc0a5ebd54ee70414b7b01620bd
Chandu900 <chandreshyaduvanshi08@gmail.com>
2024-07-05 19:20:04 +0530

    Add all my files

---
+++ Frontend/src/components/Course.jsx
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from "react";
+import Cards from "./Cards";
+import axios from "axios";
+import { Link } from "react-router-dom";
+function Course() {
+  const [book, setBook] = useState([]);
+  useEffect(() => {
+    const getBook = async () => {
+      try {
+        const res = await axios.get("http://localhost:4001/book");
+        console.log(res.data);
+        setBook(res.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getBook();
+  }, []);
+  return (
+    <>
+      <div className=" max-w-screen-2xl container mx-auto md:px-20 px-4">
+        <div className="mt-28 items-center justify-center text-center">
+          <h1 className="text-2xl  md:text-4xl">
+            We're delighted to have you{" "}
+            <span className="text-pink-500"> Here! :)</span>
+          </h1>
+          <p className="mt-12">
+            In an era dominated by technological advancements and digital transformation, traditional brick-and-mortar bookstores have found a contemporary counterpart in bookstore apps. These digital platforms have revolutionized the way readers discover, purchase, and engage with books, offering a seamless and immersive experience that transcends the boundaries of physical space. This essay explores the impact and significance of bookstore apps in today's digital landscape.
+          </p>
+          <Link to="/">
+            <button className="mt-6 bg-pink-500 text-white px-4 py-2 rounded-md hover:bg-pink-700 duration-300">
+              Back
+            </button>
+          </Link>
+        </div>
+        <div className="mt-12 grid grid-cols-1 md:grid-cols-4">
+          {book.map((item) => (
+            <Cards key={item.id} item={item} />
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Course;
